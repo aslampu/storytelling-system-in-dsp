4a722d4caba695c5686a0686ddaf3c9dff5dba2e
kkmitch <kkmitch@934d8136-0950-11df-bd4f-1fc3cb39d6f8>
2010-01-29 16:37:57 +0000

    Lab2 Matlab Part Code

---
+++ EEB586L_Lab2/Lab2.m
@@ -0,0 +1,14 @@
+clear all
+clc
+[y,Fs] = wavread('theforce_dist.wav');
+T = 1/Fs;
+L = length(y);
+NFFT = 2^nextpow2(L);
+Y = fft(y,NFFT)/L;
+f = Fs/2*linspace(0,1,NFFT/2+1);
+
+% Plot single-sided amplitude spectrum.
+plot(f,2*abs(Y(1:NFFT/2+1))) 
+title('Single-Sided Amplitude Spectrum of y(t)')
+xlabel('Frequency (Hz)')
+ylabel('|Y(f)|')
