18aaebfe0049cf10f4a7e711c0fb67977f5cd0bd
dave.robert.martin <dave.robert.martin@934d8136-0950-11df-bd4f-1fc3cb39d6f8>
2010-03-12 16:15:39 +0000

    Minor changes to ImageBlending.m

--- MATLAB/ImageBlending.m
+++ MATLAB/ImageBlending.m
@@ -99,7 +99,7 @@ new_insert_rgb = Lab2RGB(insert_lab);
 
 % Display results
 figure; hold on;
-subplot(2,2,1); imagesc(bg_image);
-subplot(2,2,2); imagesc(new_bg_rgb);
-subplot(2,2,3); imagesc(insert_image);
-subplot(2,2,4); imagesc(new_insert_rgb);
+subplot(2,2,1); imagesc(bg_image); title('Target Image');
+%subplot(2,2,2); imagesc(new_bg_rgb);
+subplot(2,2,3); imagesc(insert_image); title('Original Source Image');
+subplot(2,2,4); imagesc(new_insert_rgb); title('Blended Source Image (excluding backgound color)');
