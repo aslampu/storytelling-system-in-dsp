1b0272b1a8dc44f50c91a178b0b6e5f986188880
dave.robert.martin <dave.robert.martin@934d8136-0950-11df-bd4f-1fc3cb39d6f8>
2010-04-06 21:24:43 +0000

    HSV Hist analysis increased bin number

--- MATLAB/ImageSampleHSVHist.m
+++ MATLAB/ImageSampleHSVHist.m
@@ -27,65 +27,65 @@ wall_hsv_1 = rgb2hsv(sample_wall_1);
 figure; hold on;
 subplot(2,2,1); image(sample_blue_1); title('Blue Sample');
 t1 = blue_hsv_1(:,:,1); t1 = t1(:)'; t2 = blue_hsv_2(:,:,1); t2 = t2(:)';
-subplot(2,2,2); hist([t1 t2]); title('H Hist');
+subplot(2,2,2); hist([t1 t2], 50); title('H Hist');
 t1 = blue_hsv_1(:,:,2); t1 = t1(:)'; t2 = blue_hsv_2(:,:,2); t2 = t2(:)';
-subplot(2,2,3); hist([t1 t2]); title('S Hist');
+subplot(2,2,3); hist([t1 t2], 50); title('S Hist');
 t1 = blue_hsv_1(:,:,3); t1 = t1(:)'; t2 = blue_hsv_2(:,:,3); t2 = t2(:)';
-subplot(2,2,4); hist([t1 t2]); title('V Hist');
+subplot(2,2,4); hist([t1 t2], 50); title('V Hist');
 
 
 figure; hold on;
 subplot(2,2,1); image(sample_red_1); title('Red Sample');
 t1 = red_hsv_1(:,:,1); t1 = t1(:)'; t2 = red_hsv_2(:,:,1); t2 = t2(:)';
-subplot(2,2,2); hist([t1 t2]); title('H Hist');
+subplot(2,2,2); hist([t1 t2], 50); title('H Hist');
 t1 = red_hsv_1(:,:,2); t1 = t1(:)'; t2 = red_hsv_2(:,:,2); t2 = t2(:)';
-subplot(2,2,3); hist([t1 t2]); title('S Hist');
+subplot(2,2,3); hist([t1 t2], 50); title('S Hist');
 t1 = red_hsv_1(:,:,3); t1 = t1(:)'; t2 = red_hsv_2(:,:,3); t2 = t2(:)';
-subplot(2,2,4); hist([t1 t2]); title('V Hist');
+subplot(2,2,4); hist([t1 t2], 50); title('V Hist');
 
 
 figure; hold on;
 subplot(2,2,1); image(sample_white_2); title('White Sample');
 t1 = white_hsv_1(:,:,1); t1 = t1(:)'; t2 = white_hsv_2(:,:,1); t2 = t2(:)';
-subplot(2,2,2); hist([t1 t2]); title('H Hist');
+subplot(2,2,2); hist([t1 t2], 50); title('H Hist');
 t1 = white_hsv_1(:,:,2); t1 = t1(:)'; t2 = white_hsv_2(:,:,2); t2 = t2(:)';
-subplot(2,2,3); hist([t1 t2]); title('S Hist');
+subplot(2,2,3); hist([t1 t2], 50); title('S Hist');
 t1 = white_hsv_1(:,:,3); t1 = t1(:)'; t2 = white_hsv_2(:,:,3); t2 = t2(:)';
-subplot(2,2,4); hist([t1 t2]); title('V Hist');
+subplot(2,2,4); hist([t1 t2], 50); title('V Hist');
 
 
 figure; hold on;
 subplot(2,2,1); image(sample_black_2); title('Black Sample');
 t1 = black_hsv_1(:,:,1); t1 = t1(:)'; t2 = black_hsv_2(:,:,1); t2 = t2(:)';
-subplot(2,2,2); hist([t1 t2]); title('H Hist');
+subplot(2,2,2); hist([t1 t2], 50); title('H Hist');
 t1 = black_hsv_1(:,:,2); t1 = t1(:)'; t2 = black_hsv_2(:,:,2); t2 = t2(:)';
-subplot(2,2,3); hist([t1 t2]); title('S Hist');
+subplot(2,2,3); hist([t1 t2], 50); title('S Hist');
 t1 = black_hsv_1(:,:,3); t1 = t1(:)'; t2 = black_hsv_2(:,:,3); t2 = t2(:)';
-subplot(2,2,4); hist([t1 t2]); title('V Hist');
+subplot(2,2,4); hist([t1 t2], 50); title('V Hist');
 
 
 figure; hold on;
 subplot(2,2,1); image(sample_table_1); title('Table Sample');
 t1 = table_hsv_1(:,:,1); t1 = t1(:)'; t2 = table_hsv_2(:,:,1); t2 = t2(:)';
-subplot(2,2,2); hist([t1 t2]); title('H Hist');
+subplot(2,2,2); hist([t1 t2], 50); title('H Hist');
 t1 = table_hsv_1(:,:,2); t1 = t1(:)'; t2 = table_hsv_2(:,:,2); t2 = t2(:)';
-subplot(2,2,3); hist([t1 t2]); title('S Hist');
+subplot(2,2,3); hist([t1 t2], 50); title('S Hist');
 t1 = table_hsv_1(:,:,3); t1 = t1(:)'; t2 = table_hsv_2(:,:,3); t2 = t2(:)';
-subplot(2,2,4); hist([t1 t2]); title('V Hist');
+subplot(2,2,4); hist([t1 t2], 50); title('V Hist');
 
 
 figure; hold on;
 subplot(2,2,1); image(sample_keyboard_1); title('Keyboard Sample');
-subplot(2,2,2); hist(keyboard_hsv_1(:,:,1)); title('H Hist');
-subplot(2,2,3); hist(keyboard_hsv_1(:,:,2)); title('S Hist');
-subplot(2,2,4); hist(keyboard_hsv_1(:,:,3)); title('V Hist');
+subplot(2,2,2); hist(keyboard_hsv_1(:,:,1), 50); title('H Hist');
+subplot(2,2,3); hist(keyboard_hsv_1(:,:,2), 50); title('S Hist');
+subplot(2,2,4); hist(keyboard_hsv_1(:,:,3), 50); title('V Hist');
 
 
 figure; hold on;
 subplot(2,2,1); image(sample_wall_1); title('Wall Sample');
-subplot(2,2,2); hist(wall_hsv_1(:,:,1)); title('H Hist');
-subplot(2,2,3); hist(wall_hsv_1(:,:,2)); title('S Hist');
-subplot(2,2,4); hist(wall_hsv_1(:,:,3)); title('V Hist');
+subplot(2,2,2); hist(wall_hsv_1(:,:,1), 50); title('H Hist');
+subplot(2,2,3); hist(wall_hsv_1(:,:,2), 50); title('S Hist');
+subplot(2,2,4); hist(wall_hsv_1(:,:,3), 50); title('V Hist');
 
 
 
